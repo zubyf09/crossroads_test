4f6008a59e36b043de5c94755b9ff6a2b75ecbfb
Muhammad Zubair <zuby191@gmail.com>
2020-01-07 15:08:13 +0500

    Api Url Update

--- lib/utils/config.dart
+++ lib/utils/config.dart
@@ -8,7 +8,7 @@ class Config {
 
 
   static String getCommitsUrl() {
-    return   "https://api.github.com/repos/google/oboe/commits"; //"$baseUrl/$commithistoryUrl";zubyf09/crossroads_test
+    return   "https://api.github.com/repos/zubyf09/crossroads_test/commits"; //"$baseUrl/$commithistoryUrl";zubyf09/crossroads_test
   }
 
 }
